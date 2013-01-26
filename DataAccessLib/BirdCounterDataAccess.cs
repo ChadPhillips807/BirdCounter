da306b4ea60a9c0c253b9c79438002f854c16424
ChadPhillips807 <chadphillips807@hotmail.com>
2013-01-25 19:42:14 -0800

    Add some comments/specs to the project.

--- DataAccessLib/BirdCounterDataAccess.cs
+++ DataAccessLib/BirdCounterDataAccess.cs
@@ -11,5 +11,21 @@ namespace DataAccessLib
         // Once birds db is added to SQL
         // TODO: ADD CONNECTOIN STRING!!
         private const string ConnectionString = @"Server=localhost;Database=Birds;Integrated Security=SSPI";
+
+        //  Region where the birds were observed i. A list of regions should be retrieved
+        //  from the database and the user should be able to choose from the list.
+
+
+        //  Date of count
+
+        //  Birder (person doing the count)  A list of birders should be retrieved from the database
+        //  and the user should be able to choose from the list.
+
+        //  Bird (e.g., robin, blue jay, towhee) A list of birds should be retrieved from the database and the user
+        //  should be able to choose from the list. 
+        
+        
+        //  Number of birds observed
+
     }
 }
