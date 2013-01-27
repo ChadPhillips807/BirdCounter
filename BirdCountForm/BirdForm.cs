adbfebca907141aeca456d6c5f738df4082172bd
ChadPhillips807 <chadphillips807@hotmail.com>
2013-01-27 11:08:10 -0800

    Had to copy all the code for Querying
    Had to copy all the code for Querying data from the birds DB along with
    the code to populate the comboxes

--- BirdCountForm/BirdForm.cs
+++ BirdCountForm/BirdForm.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using DataAccessLib;
 
 namespace BirdCountForm
 {
@@ -14,6 +15,23 @@ namespace BirdCountForm
         public birdCountForm()
         {
             InitializeComponent();
+        }
+
+        private void birdCountForm_Load(object sender, EventArgs e)
+        {
+            List<string> regionQuery = new List<string>() { "Region", "RegionName" };
+            List<string> birderQuery = new List<string>() { "Birder", "FirstName" };
+            List<string> birdQuery = new List<string>() { "Bird", "Name" };
+
+
+
+            List<String> regionResults = BirdCounterDataAccess.GetData(regionQuery);
+            List<String> birderResults = BirdCounterDataAccess.GetData(birderQuery);
+            List<String> birdResults = BirdCounterDataAccess.GetData(birdQuery);
+
+            regionComboBox.DataSource = regionResults;
+            birderComboBox.DataSource = birderResults;
+            birdTypeComboBox.DataSource = birdResults;
         }
     }
 }
